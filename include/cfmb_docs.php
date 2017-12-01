e8812ad53d5f263e48bd4d714f7d0fcc7b9314f5
Swina Allen <swina.allen@gmail.com>
2017-12-01 10:23:08 +0300

    Updated documentation

--- include/cfmb_docs.php
+++ include/cfmb_docs.php
@@ -1,8 +1,14 @@
 <h1>Custom Fields MetaBox for Woocommerce</h1>
 <h2>Documentation</h2>
-
+<h4>For updated documentation check <a href="https://github.com/swina/custom-fields-meta-box-for-woocommerce/wiki" target="_blank">here</a></h4>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="PR5979HT68HGN">
+<input type="image" src="https://www.paypalobjects.com/it_IT/IT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal è il metodo rapido e sicuro per pagare e farsi pagare online.">
+<img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
+</form>
 <h3>What is Custom Fields MetaBox for Woocommerce?</h3>
-<p>Custom Fields MetaBox for Woocommerce is a useful plugin to add custom text fields to Woocommerce products.</p>
+<p>Custom Fields MetaBox for Woocommerce is a free plugin to add custom text fields to Woocommerce products.</p>
 <h3>Features</h3>
 <ul>
   <li>- Create/Disable/Delete custom text fields for Woocommerce products </li>
@@ -15,11 +21,11 @@
 </ul>
 
 <h3>How it works?</h3>
-<p>With Custom Fields MetaBox you can define as many fields you want (2 max for the free version). Once you have defined your custom fields, they will be available int the product edit page, where you can input your data. Only fields with a value will be saved to database.
+<p>With Custom Fields MetaBox you can define as many fields you want. Once you have defined your custom fields, they will be available int the product edit page, where you can input your data. Only fields with a value will be saved to database.
   <br>
   After editing your Custom Fields for the product they will be displayed in the product single page of your shop.
   <br>
-  You can decide if you custom fields have to be added to the description tab or in a new tab with a custom name (only Premium). This options are available in the Custom Fields Metabox Settings.
+  You can decide if you custom fields have to be added to the description tab or in a new tab with a custom name. This options are available in the Custom Fields Metabox Settings.
   <br>
   You can also add custom fields to the single page meta section, setting the flag <code>Product Page Meta</code> to checked.
   <br>
@@ -36,13 +42,24 @@
 </p>
 
 <h3>Is there a Premium Version?</h3>
-<p>Custom Fields Meta Box for Woocommerce is completely free. If you need direct support you can access to the Premium Service for 29$ per year.
+<p>Custom Fields Meta Box for Woocommerce is completely free.
 </p>
 <p>
-All updates are completely free.
+  If you feel good with this plugin you can even donate some!
+  <br>
+  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="PR5979HT68HGN">
+<input type="image" src="https://www.paypalobjects.com/it_IT/IT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal è il metodo rapido e sicuro per pagare e farsi pagare online.">
+<img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
+</form>
 </p>
+<h3>Development Roadmap</h3>
 <p>
-  If you fee good with this plugin you can even donate some!
+<ul>
+  <li>- create sets of common custom fields for specific products category (computer, electronic, etc.)</li>
+  <li>- create CSV product import templates as sample</li>
+</ul>
 
 </p>
 <?php
